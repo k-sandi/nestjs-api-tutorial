d7ceb85c4f48ee800878ec7bbc4d9138a0f07df7
k-sandi <kurniya097@gmail.com>
2024-02-26 02:06:58 +0700

    feature: editUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 // import { GetUser } from 'src/auth/decorator';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard) // use custom guard
 @Controller('user')
 export class UserController {
+
+    constructor(private userService: UserService) { }
 
     @Get('me')
     getMe(
@@ -15,7 +19,12 @@ export class UserController {
         return user
     }
 
-    @Patch('me')
-    editUser() { }
+    @Patch('')
+    editUser(
+        @GetUser('id') userId: number,
+        @Body() dto: EditUserDto
+    ) {
+        return this.userService.editUser(userId, dto)
+    }
 
 }
